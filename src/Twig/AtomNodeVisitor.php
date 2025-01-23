28040a0ca086e9b69ae9af07d610c54066466981
Matěj Votruba <matej.votruba@depoto.cz>
2025-01-23 14:24:02 +0100

    Update doEnterNode defaultBody

--- src/Twig/AtomNodeVisitor.php
+++ src/Twig/AtomNodeVisitor.php
@@ -9,49 +9,43 @@ use Twig\Node\Node;
 use Twig\Node\TextNode;
 use Twig\NodeVisitor\AbstractNodeVisitor;
 
+// TODO resolve AbstractNodeVisitor deprecation
 class AtomNodeVisitor extends AbstractNodeVisitor
 {
     protected NodeHelper $nodeHelper;
 
-    public function __construct(NodeHelper $nh)
+    public function __construct(NodeHelper $nodeHelper)
     {
-        $this->nodeHelper = $nh;
+        $this->nodeHelper = $nodeHelper;
     }
 
     public function doEnterNode(Node $node, Environment $env)
     {
         if ($node instanceof NodeAtom) {
             $atomName = $node->getAttribute('name');
-            $defaultBody = $node->getNode('body')->getAttribute('data');
-//            $body = $this->checkAtom($atomName, $defaultBody);
+            $defaultBody = '';
+            if ($node->getNode('body')->hasAttribute('data')) {
+                $defaultBody = $node->getNode('body')->getAttribute('data');
+            }
             $body = $this->nodeHelper->checkAtom($atomName, $defaultBody);
             $node->setNode('body', new BodyNode([
-//                new TextNode('<div class="atom" id="' . $atomName . '">' . $body . '</div>', 1),
                 new TextNode(!is_null($body) ? $body : '', 1),
-//                new PrintNode(new NameExpression($name, 1), 1),
             ]));
             $node->setAttribute('default_locale', $this->nodeHelper->getDefaultLocale());
         }
 
         if ($node instanceof NodeAtomLine) {
             $atomName = $node->getAttribute('name');
-            $defaultBody = $node->getNode('body')->getAttribute('data');
-//            $body = $this->checkAtom($atomName, $defaultBody);
+            $defaultBody = '';
+            if ($node->getNode('body')->hasAttribute('data')) {
+                $defaultBody = $node->getNode('body')->getAttribute('data');
+            }
             $body = $this->nodeHelper->checkAtomLine($atomName, $defaultBody);
             $node->setNode('body', new BodyNode([
-//                new TextNode('<div class="atom" id="' . $atomName . '">' . $body . '</div>', 1),
                 new TextNode(!is_null($body) ? $body : '', 1),
-//                new PrintNode(new NameExpression($name, 1), 1),
             ]));
         }
-//        if ($node->getNodeTag() === 'atom') {
-////            $name = $node->getAttribute('name');
-//            $node->setNode('atom', new Node([
-//                'NODE',
-//                $node->getNode('atom'),
-////                new BlockNode(Template::PROFILE_STAGE_END, Template::PROFILE_TYPE_BLOCK, $name),
-//            ]));
-//        }
+
         return $node;
     }
 
